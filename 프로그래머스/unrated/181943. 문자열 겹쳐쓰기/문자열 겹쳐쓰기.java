f7d02a58f42a1c3201ff80411a829a84760d7cb1
better-gyeom <125348113+better-gyeom@users.noreply.github.com>
2023-08-25 13:25:30 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 1.19 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -3,9 +3,8 @@ class Solution {
         String answer = "";
 
 //         String change = my_string.substring(s, s+ overwrite_string.length());
-//         // System.out.println(change);
+//         answer = my_string.replace(change, overwrite_string); // replace는 앞에서부터 단어를 찾는다. -> 고로 똑같은 단어가 앞에서 부터 반복되면 다른 답이 나올 수 있다.
         
-//         answer = my_string.replace(change, overwrite_string);
         answer = my_string.substring(0, s);
         answer += overwrite_string;
         answer += my_string.substring(s + overwrite_string.length(), my_string.length());
