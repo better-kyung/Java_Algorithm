3ae3bea9fcc329a3e7f7bad9fd94f175503d5a5b
better-gyeom <125348113+better-gyeom@users.noreply.github.com>
2023-08-30 15:48:50 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 2.53 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        
+        for(int[] query : queries) {
+            for(int i = query[0]; i <= query[1]; i++) {
+                if(i % query[2] == 0) {
+                    arr[i] += 1;
+                    
+                }
+            }
+            
+        }
+        
+        answer = arr;
+        
+        return answer;
+    }
+}
