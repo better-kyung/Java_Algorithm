5abc11d945a0ef472414a0b69db05b0184372416
better-gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-01-16 20:40:23 +0900

    [level Lv.1] Title: 최대공약수와 최소공배수, Time: 0.12 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = {1, 1};
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0 && m % i == 0) {
+                if (i > answer[0]) {
+                    answer[0] = i;
+                }
+            }
+        }
+        
+        answer[1] = (n * m) / answer[0];
+        
+        
+        
+        return answer;
+    }
+}
