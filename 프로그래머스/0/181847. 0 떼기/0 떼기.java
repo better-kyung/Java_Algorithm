65e7d72b72972cbbb6c8c7f7a561cdb2e625ee87
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-04-10 22:43:52 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int temp = Integer.valueOf(n_str);
+        answer = String.valueOf(temp);
+        return answer;
+    }
+}
